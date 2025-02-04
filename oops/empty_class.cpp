b2b479ff7e6a67f1b9ee66fbd873529ebb675ce0
yasir <mcayasir1501@gmail.com>
2025-02-04 18:54:25 +0530

    emplty class work

--- oops/empty_class.cpp
+++ oops/empty_class.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-class abc {
-    int val ; 
+class abc
+{
+    int val;
     char arr[20];
     static int count; // size of static variable is not included in the size of class object
 };
@@ -13,11 +14,27 @@ class abc {
 // class abc{
 //     public:
 //     int a; //if value is not assigned defualt value is 0
+// if private then it give complie time error
 // };
 
+class student
+{
+public:
+    int roll;
+    int age;
+};
 
-int main(){
+int main()
+{
     abc obj;
-    cout<<sizeof(obj)<<endl;
+    cout << sizeof(obj) << endl;
+    student s1;
+    student const s2 = s1;
+    student s3 = s1;
+    s1.roll = 101;
+    s1.age = 20;
+    cout << s1.roll << " " << s1.age << endl;
+    cout << s2.roll << " " << s2.age << endl; // garbage value because s2 is const object and we are not assigning value to it
+    cout << s3.roll << " " << s3.age << endl; // garbage
     return 0;
 }
--- oops/tempCodeRunnerFile.cpp
+++ oops/tempCodeRunnerFile.cpp
@@ -1,1 +1,1 @@
-cgpaptr
+}
