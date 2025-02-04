5ec7632cf69bb49a1f9e28f4c3afabc1814f67b1
yasir <mcayasir1501@gmail.com>
2025-02-04 19:50:39 +0530

    constructor

--- oops/constructor.cpp
+++ oops/constructor.cpp
@@ -14,16 +14,31 @@ public:
     string name;
     string dept;
     int age;
-    teacher(){
-        cout<<"i am theppp constructor"<<endl;
+    teacher()
+    {
+        cout << "i am theppp constructor" << endl;
+    }
+};
+
+class student
+{   public:
+    int i ; 
+    student()
+    {
+        cout << "i am the default  constructor" << endl;
+    }
+
+    student(int val =0)
+    {
+        cout << "i am the  parameterized constructor "<<val << endl;
     }
 };
 
 int main()
-{
+{      // student s; //ambiguity error because we have two constructor
+        student s1(10); //use parameterized constructor with given value
 
     teacher ashok();
-
 
     return 0;
 }
